e088f5ffa907c5c734ea6e2c787db8d20ce6a29a
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-11-19 23:24:12 -0400

    List data from db

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ mongoose
   .connect(url, {})
   .then(() => console.log("Connection successfully!"))
   .catch((e) => console.log("Error! " + e));
+
+const peopleSchema = mongoose.Schema({
+    name:String,
+    age:Number,
+    country:String
+})
+
+const peopleModel = mongoose.model('people', peopleSchema)
+
+// List
+const listData = async () => {
+    const people = await peopleModel.find()
+    console.log(people);
+}
